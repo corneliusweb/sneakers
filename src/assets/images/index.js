327fb7b1ce5b5c588aa8633f1f82f171f5cceb30
Cornelius Asogwa <cornelius.webdev@gmail.com>
2025-08-16 07:32:00 +0100

    Create index files for vectors & images barrel export

---
+++ src/assets/images/index.js
@@ -0,0 +1,21 @@
+import avatar from './image-avatar.png';
+import imageProduct1 from './image-product-1.jpg';
+import imageProduct2 from './image-product-2.jpg';
+import imageProduct3 from './image-product-3.jpg';
+import imageProduct4 from './image-product-4.jpg';
+import imageProduct1Thumbnail from './image-product-1-thumbnail.jpg';
+import imageProduct2Thumbnail from './image-product-2-thumbnail.jpg';
+import imageProduct3Thumbnail from './image-product-3-thumbnail.jpg';
+import imageProduct4Thumbnail from './image-product-4-thumbnail.jpg';
+
+export {
+	avatar,
+	imageProduct1,
+	imageProduct2,
+	imageProduct3,
+	imageProduct4,
+	imageProduct1Thumbnail,
+	imageProduct2Thumbnail,
+	imageProduct3Thumbnail,
+	imageProduct4Thumbnail,
+};
---
+++ src/assets/vectors/index.js
@@ -0,0 +1,21 @@
+import logo from './logo.svg';
+import cartIcon from './icon-cart.svg';
+import closeIcon from './icon-close.svg';
+import deleteIcon from './icon-delete.svg';
+import menuIcon from './icon-menu.svg';
+import minusIcon from './icon-minus.svg';
+import plusIcon from './icon-plus.svg';
+import nextIcon from './icon-next.svg';
+import previousIcon from './icon-previous.svg';
+
+export {
+	logo,
+	cartIcon,
+	closeIcon,
+	deleteIcon,
+	menuIcon,
+	minusIcon,
+	plusIcon,
+	nextIcon,
+	previousIcon,
+};
