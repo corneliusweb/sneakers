395603a29040f78b79d059820c06571abf3f4ebf
Cornelius Asogwa <cornelius.webdev@gmail.com>
2025-08-17 08:37:57 +0100

    Create constants/ and add product images

---
+++ src/constants/index.ts
@@ -0,0 +1,17 @@
+import {
+	imageProduct1,
+	imageProduct2,
+	imageProduct3,
+	imageProduct4,
+	imageProduct1Thumbnail,
+	imageProduct2Thumbnail,
+	imageProduct3Thumbnail,
+	imageProduct4Thumbnail,
+} from '../assets/images';
+
+export const shoes = [
+	{ bigShoe1: imageProduct1, thumbnail: imageProduct1Thumbnail },
+	{ bigShoe2: imageProduct2, thumbnail: imageProduct2Thumbnail },
+	{ bigShoe3: imageProduct3, thumbnail: imageProduct3Thumbnail },
+	{ bigShoe4: imageProduct4, thumbnail: imageProduct4Thumbnail },
+];
