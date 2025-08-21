d55a65207401bf32198aceebc171b32499f2b3e2
Cornelius Asogwa <cornelius.webdev@gmail.com>
2025-08-21 11:03:24 +0100

    Implement cart design for mobile screen

--- src/components/CartModal.tsx
+++ src/components/CartModal.tsx
@@ -5,8 +5,10 @@ const CartModal = () => {
 	const { items, removeFromCart } = useCartContext();
 
 	return (
-		<div className='absolute top-12 right-0 bg-white w-80 p-4 rounded shadow-lg'>
-			<h1 className='font-bold mb-4'>Cart</h1>
+		<div className='absolute z-50 top-12 right-0 bg-white w-90 p-4 rounded shadow-lg  max-mobile:top-18 max-mobile:min-h-56 max-mobile:-right-2'>
+			<h1 className='font-bold mb-4 max-mobile:mb-5 max-mobile:pb-5 max-mobile:border-b-2 max-mobile:border-black/10'>
+				Cart
+			</h1>
 			{items.length === 0 ? (
 				<p className='text-center text-gray-500 py-8'>Your cart is empty</p>
 			) : (
@@ -29,7 +31,10 @@ const CartModal = () => {
 									).toFixed(2)}
 								</b>
 							</div>
-							<button className='cursor-pointer' onClick={() => removeFromCart(item.product.id)}>
+							<button
+								className='cursor-pointer'
+								onClick={() => removeFromCart(item.product.id)}
+							>
 								🗑️
 							</button>
 						</div>
